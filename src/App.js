0fef3cba3a1512fd0591220994d04154150c8839
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-01-28 16:26:26 +0100

    add const filterItems

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { useState } from 'react';
-import Menu from './Menu';
-import Categories from './Categories';
-import items from './data';
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState([]);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
@@ -14,11 +24,11 @@ function App() {
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
-        <Categories />
-        <Menu />
+        <Categories filterItems={filterItems} />
+        <Menu items={menuItems} />
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
